6beeeb9de4c74c15d20ad9575b911dda9a78dc32
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-09-16 17:27:46 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,89 @@
-import Image from "next/image";
-import SwaggerUI from 'swagger-ui-react';
-import 'swagger-ui-react/swagger-ui.css';
+'use client';
+
+import Script from 'next/script';
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SwaggerUI url="/openapi.yaml" />
+    <main style={styles.main}>
+      <div style={styles.container}>
+        <h1 style={styles.heading}>Welcome to Postman Collection</h1>
+        <p style={styles.description}>
+          Click the button below to fork the Postman collection and start using it.
+        </p>
+
+        <div
+          className="postman-run-button"
+          data-postman-action="collection/fork"
+          data-postman-visibility="private"
+          data-postman-var-1="23738835-1d348aac-a84d-4a5f-9a36-90089b69cd6a"
+          data-postman-collection-url="entityId=23738835-1d348aac-a84d-4a5f-9a36-90089b69cd6a&entityType=collection&workspaceId=9824cab5-ac46-45d4-8c55-812e888baf45"
+          style={styles.button}
+        ></div>
+        <Script
+        src="https://run.pstmn.io/button.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          (function (p, o, s, t, m, a, n) {
+            if (!p[s]) {
+              p[s] = function () {
+                (p[t] || (p[t] = [])).push(arguments);
+              };
+            }
+            if (!o.getElementById(s + t)) {
+              const script = o.createElement('script');
+              script.id = s + t;
+              script.async = true;
+              script.src = m;
+              o.head.appendChild(script);
+            }
+          })(window, document, '_pm', 'PostmanRunObject', 'https://run.pstmn.io/button.js');
+        }}
+      />
+      </div>
     </main>
   );
 }
+
+const styles = {
+  main: {
+    display: 'flex',
+    minHeight: '100vh',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: '20px',
+  },
+  container: {
+    backgroundColor: '#ffffff',
+    padding: '40px',
+    borderRadius: '10px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    textAlign: 'center',
+    maxWidth: '500px',
+    width: '100%',
+  },
+  heading: {
+    fontSize: '2rem',
+    fontWeight: 'bold',
+    marginBottom: '20px',
+    color: '#333',
+  },
+  description: {
+    fontSize: '1.1rem',
+    color: '#666',
+    marginBottom: '30px',
+  },
+  button: {
+    // display: 'inline-block',
+    // padding: '15px 30px',
+    fontSize: '1rem',
+    backgroundColor: '#007bff',
+    color: '#ffffff',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    border: 'none',
+    boxShadow: '0 3px 6px rgba(0, 0, 0, 0.1)',
+    transition: 'background-color 0.3s ease',
+  },
+};
