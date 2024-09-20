4d5fbf670d5542d23d2bf7caccbedacb8e8d5a61
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-09-20 12:14:11 +0700

    update usernaewm

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,13 +1,30 @@
+import { generateUsername } from "@tutor/app/utils/auth";
 import { supabaseApi } from "@tutor/supabase/apiRouteClient";
 import { NextResponse } from "next/server";
 
 // Xử lý POST request - Đăng ký người dùng
 export async function POST(req) {
-  const { username, email, password, role, phone = "", is_active=false } = await req.json(); // Lấy dữ liệu từ request body
+  const { full_name, email, password, role, phone = "", is_active = false } = await req.json(); // Lấy dữ liệu từ request body
+
+  // Kiểm tra dữ liệu đầu vào
+  if (!full_name || !email || !password) {
+    return NextResponse.json({ error: "Thiếu thông tin đăng ký" }, { status: 400 });
+  }
+
+  let { username, username_add } = generateUsername(full_name); // Tạo username từ họ tên
+  // kieem tra username đã tồn tại chưa
+  let { data: userCheck, error: errorCheck } = await supabaseApi.from('user').select('id').eq('username', username);
+  if (errorCheck) {
+    return NextResponse.json({ error: errorCheck.message }, { status: 500 });
+  }
+
+  if (userCheck.length > 0) {
+    username = username_add;
+  }
 
   // Tạo tài khoản người dùng
   const { error: insertError } = await supabaseApi.from('user').insert([
-    { username, email, password, role, is_active: true }
+    { username, full_name, email, password, role, is_active, phone }
   ]);
 
   if (insertError) {
--- src/app/utils/auth.js
+++ src/app/utils/auth.js
@@ -25,3 +25,27 @@ export const validateToken = async (req) => {
       return null;
     }
 }
+
+export function generateUsername(fullName) {
+  // Chuyển họ tên về mảng các từ
+  let nameParts = fullName.split(' ');
+  
+  // Lấy họ (từ đầu tiên) và tên (từ cuối cùng)
+  let lastName = nameParts[0];
+  let firstName = nameParts[nameParts.length - 1];
+  
+  // Tạo username từ họ và tên, loại bỏ dấu
+  let username = (lastName + firstName)
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")  // Loại bỏ dấu
+      .replace(/[^a-zA-Z0-9]/g, '');   // Loại bỏ ký tự đặc biệt
+
+  // Thêm chuỗi ngẫu nhiên từ a-z, A-Z, 0-9 để đảm bảo tính duy nhất
+  let randomString = Math.random().toString(36).substring(2, 8); // Tạo chuỗi ngẫu nhiên 6 ký tự
+
+  return {
+    username: username,
+    username_add: username + randomString
+  }
+}
